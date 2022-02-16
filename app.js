30c585ab250e0c306958539567b078b7ed6989ca
Omr-pseudo <uali2407@gmail.com>
2022-02-16 19:52:02 +0500

    Fix: changed db connection

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
-const path = require('path')
+const path = require('path');
 
 
 //-------------------------------------------Routes---------------------------------------------------------------------
