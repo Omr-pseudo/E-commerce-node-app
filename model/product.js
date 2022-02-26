781f5c642f650dcff187647fa8ee41d57205dbe8
Omr-pseudo <uali2407@gmail.com>
2022-02-26 22:32:25 +0500

    feature added mongoose product schema and model exported

--- model/product.js
+++ model/product.js
@@ -1,97 +1,25 @@
-const {getdbClient} = require('../utilities/database');
+const mongoose = require('mongoose');
 
-const mongodb = require('mongodb');
+const Schema = mongoose.Schema;
 
-
-class Product {
-
-  constructor(title, price, imageURL, description, id, userId){
-
-    this.title = title,
-    this.price = price,
-    this.imageURL = imageURL,
-    this.description = description
-    this._id = id? new mongodb.ObjectId(id): null;
-    this.userId = userId;
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description:{
+    type: String,
+    required: true
+  },
+  imageURL: {
+    type: String,
+    required: true
   }
+});
 
-  save(){
 
-    const db = getdbClient();
-    let dbOp;
-
-    if(this._id){
-
-      //updating 
-
-      dbOp = db.collection('products').updateOne({_id: this._id}, {$set: this});
-
-    }
-
-    else{
-
-      //insert new
-
-      dbOp = db.collection('products').insertOne(this);
-    }
-
-    return dbOp
-    .then( result => {
-      console.log(result);
-    })
-    
-    .catch( err => {
-      console.log(err);
-    });
-  }
-
-  static fetchAll(){
-
-    const db = getdbClient();
-
-    return db.collection('products').find().toArray()
-    .then( products => {
-      console.log("FETCHED !!!");
-      return products;
-    })
-    .catch( err => {
-      console.log(err);
-    });
-  }
-
-  static findById(prod_id){
-
-    const db = getdbClient();
-
-    return db.collection('products').find({ _id: new mongodb.ObjectId(prod_id)})
-    .next()
-
-    .then( product => {
-      console.log(product);
-      return product;
-    })
-
-    .catch( err => {
-      console.log(err);
-    });
-  }
-
-  static deleteById(prod_id){
-
-    const db = getdbClient();
-
-    return db.collection('products')
-    .deleteOne({_id: new mongodb.ObjectId(prod_id)})
-
-    .then( result => { 
-
-      console.log("Deleted !!");
-    })
-
-    .catch( err => {
-      console.log(err);
-    })
-  }
-}
-
-module.exports = Product;
+module.exports = mongoose.model('Product', productSchema);
