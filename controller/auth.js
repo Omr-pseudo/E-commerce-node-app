9efa444fa913f77c66a2b6f793ec612d69f759e3
Omr-pseudo <uali2407@gmail.com>
2022-03-02 22:25:13 +0500

    Feature added user authentication

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,7 @@
 const User = require('../model/user');
 
+const bcrypt = require('bcryptjs');
+
 exports.getLogin = (req,res,next) => {
 
     
@@ -13,14 +15,38 @@ exports.getLogin = (req,res,next) => {
 
 exports.postLogin = (req,res,next) => {
 
-    User.findById('621b9724522142874834b81c')
-    .then(user => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email: email}).then( user => {
+
+    return bcrypt.compare(password, user.password).then( matched => {
+
+      if(!matched){
+
+        return res.redirect('/login');
+      }
+
+
       req.session.isLoggedIn = true;
       req.session.user = user;
       req.session.save(err => {
         console.log(err);
         res.redirect('/');
       });
+
+
+    }).catch( err => {
+
+      console.log(err);
+
+      res.redirect('/login');
+    })
+  })
+
+    User.findById('621b9724522142874834b81c')
+    .then(user => {
+      
     })
     .catch(err => console.log(err));
 }
@@ -37,3 +63,75 @@ exports.postLogout = (req,res,next) => {
     
     
 }
+
+exports.getSignup = (req, res, next) => {
+
+  res.render('auth/signup-form', { 
+    myTitle: 'Signup', 
+     path:"/signup",
+     isAuthenticated: req.session.isLoggedIn
+    });
+  }
+
+exports.postSignup = (req, res, next) => {
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPass = req.body.confirm_password;
+
+  User.findOne({email: email})
+  .then( userDoc => {
+
+    if(userDoc) {
+
+      return res.redirect('/signup');
+    }
+
+    return bcrypt.hash(password, 12).then( hashedPassword => {
+
+      const newUser = new User(
+        {
+          email: email,
+          password: hashedPassword, 
+          cart: {items:[]}
+          });
+
+      return newUser.save()
+
+    }).then( result => {
+
+      res.redirect('/login');
+    })
+
+  }).catch( err => {
+
+    console.log(err);
+  })
+}
+
+
+
+//Setting a cookie
+
+
+/*
+
+exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get('Cookie')
+    .split(';')[1]
+    .trim()
+    .split('=')[1];
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: isLoggedIn
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
+};
+
+*/
